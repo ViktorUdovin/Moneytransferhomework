6d389fadfdda52898cdde6f5c429b886c7a75d0e
ViktorUdovin <viktorudovinwork@gmail.com>
2020-04-20 13:57:14 +0400

    added all fails

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+	int price = 2_000_000_000;
+	System.out.println("price: " + price);
+	int count = 500_000_000;
+	System.out.println("count: " + count);
+	int total = price + count;
+	System.out.println("total: " + total);
+    }
+}
