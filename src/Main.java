927b5cd7387fe17d26baef0beac47157d09b147e
ViktorUdovin <viktorudovinwork@gmail.com>
2020-04-20 14:00:57 +0400

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-	int price = 2_000_000_000;
-	System.out.println("price: " + price);
-	int count = 500_000_000;
-	System.out.println("count: " + count);
-	int total = price + count;
+	int score = 2_000_000_000;
+	System.out.println("score: " + score);
+	int receipt = 500_000_000;
+	System.out.println("receipt: " + receipt);
+	int total = score + receipt;
 	System.out.println("total: " + total);
     }
 }
