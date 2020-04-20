9286d06c132d92258ca0546653f38b3c8706e9de
ViktorUdovin <viktorudovinwork@gmail.com>
2020-04-20 15:01:30 +0400

    added report

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-	int score = 2_000_000_000;
+	long score = 2_000_000_000;
 	System.out.println("score: " + score);
-	int receipt = 500_000_000;
+	long receipt = 500_000_000;
 	System.out.println("receipt: " + receipt);
-	int total = score + receipt;
+	long total = score + receipt;
 	System.out.println("total: " + total);
     }
 }
